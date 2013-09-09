25855d5d96af89ec66b9e36718432cc0135cc9de
Pedro Jordano <pedroj@me.com>
2013-09-10 01:15:59 +0200

    Update d3Network pages

--- d3Network/README.md
+++
@@ -1,4 +0,0 @@
-d3Network_test
-==============
-
-Web pages for the tests using d3Network with bipartite networks.
--- d3Network/d3Network_files/d3Network.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20130909
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('d3Network_files/d3NetworkMoz.css')
-detectBrowser();Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
