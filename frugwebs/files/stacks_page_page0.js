d87c72111324521586b4e89e707fa686ec50908f
Pedro Jordano <jordano@ebd.csic.es>
2016-11-07 01:13:28 +0100

    Edited frugweb page

---
+++ frugwebs/files/stacks_page_page0.js
@@ -0,0 +1,19 @@
+
+var stacks = {};
+stacks.jQuery = jQuery.noConflict(true);
+stacks.stacks_in_11_page0 = {};
+stacks.stacks_in_11_page0 = (function(stack) {
+var jQuery = stacks.jQuery;var $ = jQuery;
+
+
+
+
+return stack;})(stacks.stacks_in_11_page0);
+stacks.stacks_in_12_page0 = {};
+stacks.stacks_in_12_page0 = (function(stack) {
+var jQuery = stacks.jQuery;var $ = jQuery;
+
+
+
+
+return stack;})(stacks.stacks_in_12_page0);
